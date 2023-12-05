47e0ae02dbd09acae569643976123a675901ebe2
brensen11 <90063365+brensen11@users.noreply.github.com>
2023-12-04 17:49:43 -0700

    try except around server.status().players.sample ...

--- mc_serverstat/ServerBot.py
+++ mc_serverstat/ServerBot.py
@@ -9,7 +9,11 @@ BOT_NAME = None
 
 def update_players(server: JavaServer):
     global players
-    curr_players_sample = server.status().players.sample
+    try:
+        curr_players_sample = server.status().players.sample
+    except:
+        print("Error in server connection ...", flush=True)
+        return
     curr_players_sample = curr_players_sample if curr_players_sample is not None else []  # sample can be None
     for p in curr_players_sample:
         if p in players:  # nothing happens, they are still online
@@ -17,12 +21,12 @@ def update_players(server: JavaServer):
         else:  # player p joined!
             msg = f"{p.name} joined"
             msg_server(msg)
-            print(msg)
+            print(msg, flush=True)
     # what's left in players, left!
     for p in players:
         msg = f"{p.name} left"
         msg_server(msg)
-        print(msg)
+        print(msg, flush=True)
     # swap players to be new_players
     players = curr_players_sample
 
@@ -37,7 +41,7 @@ def msg_server(msg):
 
 def run():
     server = JavaServer.lookup(MC_URL)
-    print("Server is Running...")
+    print("Server is Running...", flush=True)
     while True:
         update_players(server)
         time.sleep(5)
@@ -45,7 +49,7 @@ def run():
 
 def main():
     if not (3 <= len(sys.argv) <= 4):
-        print("Must run in format ServerBot.py MINECRAFT_URL DISCORD_WEBHOOK")
+        print("Must run in format ServerBot.py MINECRAFT_URL DISCORD_WEBHOOK", flush=True)
         return
     global MC_URL
     global DISCORD_WEBHOOK
